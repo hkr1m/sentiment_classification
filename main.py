ea55126a6e8e56ba268423b4208647521898f569
hkr1m <hakure1mu@126.com>
2023-05-04 00:14:48 +0800

    BiRNN/LSTM/GRU

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
 import numpy as np
 import torch
-import torch.nn.functional as F
 from torch import nn
 from torch.utils.data import Dataset, DataLoader
 from gensim.models import KeyedVectors
-import os
-import pickle
-from sklearn.metrics import f1_score
+import os, pickle, argparse
 
 class Config(object):
-    def __init__(self, word2vec):
-        self.load_model = False
-        self.learning_rate = 1e-3
-        self.batch_size = 50
-        self.epochs = 100
-        self.dropout_rate = 0.3
+    def set(self):
+        if self.device == "auto":
+            self.device = (
+                "cuda"
+                if torch.cuda.is_available()
+                else "mps"
+                if torch.backends.mps.is_available()
+                else "cpu"
+            )
+        print(f"Using {self.device} device")
+        if self.emb:
+            self.embedding = get_embedding(embedding_path=embedding_path,
+                                           word2vec_path=word2vec_path,
+                                           word2id=word2id)
+            self.vocab_size = self.embedding.size(0)
+            self.embedding_dim = self.embedding.size(1)
+        else:
+            self.vocab_size = len(word2id)
+            self.embedding_dim = 50
         self.num_class = 2
-        self.pretrained_embed = True
-        self.embedding = word2vec
-        self.vocab_size = embedding.shape[0]
-        self.embedding_dim = embedding.shape[1]
-        self.feature_size = 20
-        self.window_sizes = [3, 5, 7]
-        self.max_sent_len = 120
-        self.num_layers = 2
-        self.hidden_dim = 100
 
 class SemtimentDataset(Dataset):
     def __init__(self, data_path, word2id, max_sent_len):
@@ -58,7 +59,7 @@ class TextCNN(nn.Module):
         self.config = config
         self.embedding = nn.Embedding(num_embeddings=config.vocab_size,
                                       embedding_dim=config.embedding_dim)
-        if config.pretrained_embed:
+        if config.emb:
             self.embedding.weight.data.copy_(config.embedding)
         self.convs = nn.ModuleList([
             nn.Sequential(nn.Conv1d(in_channels=config.embedding_dim,
@@ -99,7 +100,7 @@ class BiRNN(nn.Module):
         self.config = config
         self.embedding = nn.Embedding(num_embeddings=config.vocab_size,
                                       embedding_dim=config.embedding_dim)
-        if config.pretrained_embed:
+        if config.emb:
             self.embedding.weight.data.copy_(config.embedding)
         self.fwd_rnn = nn.ModuleList([_RNNCell(input_size=config.embedding_dim,
                                                    hidden_size=config.hidden_dim)])
@@ -135,9 +136,9 @@ class BiRNN(nn.Module):
         hidden = torch.cat((h_fwd[-1], h_bwd[-1]), dim=1)
         return self.fc(hidden)
     
-class RNN_LSTM(nn.Module):
+class LSTM(nn.Module):
     def __init__(self, config):
-        super(RNN_LSTM, self).__init__()
+        super(LSTM, self).__init__()
         self.config = config
         self.embedding = nn.Embedding(num_embeddings=config.vocab_size,
                                       embedding_dim=config.embedding_dim)
@@ -149,7 +150,7 @@ class RNN_LSTM(nn.Module):
             bidirectional=True,
             batch_first=True
         )
-        if config.pretrained_embed:
+        if config.emb:
             self.embedding.weight.data.copy_(config.embedding)
         self.fc = nn.Sequential(nn.Linear(config.hidden_dim * 2, 64),
                                 nn.Linear(64, config.num_class))
@@ -161,9 +162,9 @@ class RNN_LSTM(nn.Module):
         hidden = torch.cat(hidden[-1].unbind(0), dim=-1)
         return self.fc(hidden)
 
-class RNN_GRU(nn.Module):
+class GRU(nn.Module):
     def __init__(self, config):
-        super(RNN_GRU, self).__init__()
+        super(GRU, self).__init__()
         self.config = config
         self.embedding = nn.Embedding(num_embeddings=config.vocab_size,
                                       embedding_dim=config.embedding_dim)
@@ -175,7 +176,7 @@ class RNN_GRU(nn.Module):
             bidirectional=True,
             batch_first=True
         )
-        if config.pretrained_embed:
+        if config.emb:
             self.embedding.weight.data.copy_(config.embedding)
         self.fc = nn.Sequential(nn.Linear(config.hidden_dim * 2, 64),
                                 nn.Linear(64, config.num_class))
@@ -187,10 +188,10 @@ class RNN_GRU(nn.Module):
         hidden = torch.cat(hidden[-1].unbind(0), dim=-1)
         return self.fc(hidden)
 
-def get_word2id(wordid_path, data_paths = []):
-    print("load word2id...")
+def get_word2id(word2id_path, data_paths, rebuild = False):
+    print("Loading word2id...")
     word2id = {"_NULL_": 0}
-    if len(data_paths) > 0:
+    if rebuild or not os.path.exists(word2id_path) or os.path.getsize(word2id_path) == 0:
         for path in data_paths:
             with open(path, "r", encoding="utf-8") as file:
                 for line in file:
@@ -198,16 +199,16 @@ def get_word2id(wordid_path, data_paths = []):
                     for word in words[1:]:
                         if word not in word2id.keys():
                             word2id[word] = len(word2id)
-        with open(wordid_path, "wb") as file:
+        with open(word2id_path, "wb") as file:
             pickle.dump(word2id, file)
     else:
-        with open(wordid_path, "rb") as file:
+        with open(word2id_path, "rb") as file:
             word2id = pickle.load(file)
     return word2id
 
-def get_embedding(embedding_path, word2vec_path = "", word2id = {}):
-    print("load embedding...")
-    if len(word2vec_path) > 0:
+def get_embedding(embedding_path, word2vec_path, word2id, rebuild = False):
+    print("Loading embedding...")
+    if rebuild or not os.path.exists(embedding_path) or os.path.getsize(embedding_path) == 0:
         vocab_size = len(word2id)
         model = KeyedVectors.load_word2vec_format(word2vec_path, binary=True)
         embedding = np.random.uniform(-1., 1., [vocab_size, model.vector_size])
@@ -219,13 +220,13 @@ def get_embedding(embedding_path, word2vec_path = "", word2id = {}):
         np.save(embedding_path, embedding)
     else:
         embedding = np.load(embedding_path)
-    return torch.Tensor(embedding)
+    return torch.Tensor(embedding).to(config.device)
 
 def train_loop(dataloader, model, loss_fn, optimizer):
     model.train()
     size = len(dataloader.dataset)
     for batch, (X, y) in enumerate(dataloader):
-        X, y = X.to(device), y.to(device)
+        X, y = X.to(config.device), y.to(config.device)
         optimizer.zero_grad()
         pred = model(X)
         loss = loss_fn(pred, y)
@@ -236,38 +237,14 @@ def train_loop(dataloader, model, loss_fn, optimizer):
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
     scheduler.step()
 
-# def test_loop(dataloader, model, loss_fn):
-#     model.eval()
-#     # 验证过程
-#     val_loss, val_acc = 0.0, 0.0
-#     count, correct = 0, 0
-#     full_true = []
-#     full_pred = []
-#     for _, (x, y) in enumerate(dataloader):
-#         x, y = x.to(device), y.to(device)
-#         output = model(x)
-#         loss = loss_fn(output, y)
-#         val_loss += loss.item()
-#         correct += (output.argmax(1) == y).float().sum().item()
-#         count += len(x)
-#         full_true.extend(y.cpu().numpy().tolist())
-#         full_pred.extend(output.argmax(1).cpu().numpy().tolist())
-#     val_loss *= config.batch_size
-#     val_loss /= len(dataloader.dataset)
-#     val_acc = correct / count
-#     f1 = f1_score(np.array(full_true), np.array(full_pred), average="binary")
-#     print(f"val_loss: {val_loss:>4f}, val_acc: {val_acc:>4f}, f1: {f1:>4f}")
-#     return val_loss, val_acc, f1
-
 def test_loop(dataloader, model, loss_fn):
     model.eval()
-    size = len(dataloader.dataset)
     num_batches = len(dataloader)
     test_loss = 0.
     FP, TP, FN, TN, P, N = 0, 0, 0, 0, 0, 0
     with torch.no_grad():
         for X, y in dataloader:
-            X, y = X.to(device), y.to(device)
+            X, y = X.to(config.device), y.to(config.device)
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
             tf = pred.argmax(1) == y
@@ -286,28 +263,44 @@ def test_loop(dataloader, model, loss_fn):
         recall = TP / P
         accuracy = (TP+TN) / (P+N)
         F_measure = 2 / (1/precision + 1/recall)
-        print(f"Test Error: \n\
+        print(f"Test Loss: {test_loss:>6f}\n\
   Precision: {precision:>6f}, Recall: {recall:>6f} \n\
   Accuracy: {accuracy:>6f}, F_measure: {F_measure:>6f}")
     except ZeroDivisionError:
-        print("Divide zero in F-mesure calculation")
+        print("Divide zero in F-measure calculation")
+
+class Parser(argparse.ArgumentParser):
+    def __init__(self):
+        super(Parser, self).__init__(description="Sentiment Classification")
+        self.add_argument("-m", "--model", choices=["TextCNN", "BiRNN", "LSTM", "GRU"], default="TextCNN")
+        self.add_argument("-l", "--model-load-path", dest="model_load_path", default="")
+        self.add_argument("-s", "--model-save-path", dest="model_save_path", default="")
+        self.add_argument("--device", choices=["cuda", "mps", "cpu", "auto"], default="auto")
+        self.add_argument("-b", "--batch-size", dest="batch_size", type=int, default=50)
+        self.add_argument("--lr", dest="learning_rate", type=float, default=1e-3)
+        self.add_argument("-e", "--epochs", type=int, default=10)
+        self.add_argument("-d", "--dropout-rate", dest="dropout_rate", type=float, default=0.3)
+        self.add_argument("--unemb", dest="emb", action="store_false", default=True)
+        self.add_argument("-len", "--max-sent-len", dest="max_sent_len", type=int, default=120)
+        self.add_argument("-f", "--feature-size", dest="feature_size", type=int, default=20)
+        self.add_argument("-w", "--window-sizes", dest="window_sizes", type=int, nargs="*", default=[3, 5, 7])
+        self.add_argument("-n", "--num-layers", dest="num_layers", type=int, default=2)
+        self.add_argument("-hd", "--hidden-dim", dest="hidden_dim", type=int, default=100)
 
 if __name__ == "__main__":
-    file_path = os.path.abspath(os.path.realpath(__file__))
-    root_path = file_path[:file_path.find("src")]
-    train_path =os.path.join(root_path, "data/train.txt")
-    validation_path = os.path.join(root_path, "data/validation.txt")
-    test_path = os.path.join(root_path, "data/test.txt")
-    word2id_path = os.path.join(root_path, "src/word2id.pkl")
-    embedding_path = os.path.join(root_path, "src/embedding.npy")
-    word2vec_path = os.path.join(root_path, "src/wiki_word2vec_50.bin")
-    model_path = os.path.join(root_path, "src/model.pth")
-    word2id = get_word2id(wordid_path=word2id_path,
+    train_path = "data/train.txt"
+    validation_path = "data/validation.txt"
+    test_path = "data/test.txt"
+    word2id_path = "src/word2id.pkl"
+    embedding_path = "src/embedding.npy"
+    word2vec_path = "src/wiki_word2vec_50.bin"
+    
+    config = Config()
+    Parser().parse_args(namespace=config)
+    word2id = get_word2id(word2id_path=word2id_path,
                           data_paths=[train_path, validation_path, test_path])
-    embedding = get_embedding(embedding_path=embedding_path,
-                              word2vec_path=word2vec_path,
-                              word2id=word2id)
-    config = Config(embedding)
+    config.set()
+
     training_data = SemtimentDataset(train_path, word2id, config.max_sent_len)
     validation_data = SemtimentDataset(validation_path, word2id, config.max_sent_len)
     testing_data = SemtimentDataset(test_path, word2id, config.max_sent_len)
@@ -315,21 +308,17 @@ if __name__ == "__main__":
     valid_dataloder = DataLoader(validation_data, batch_size=config.batch_size, shuffle=True)
     test_dataloader = DataLoader(testing_data, batch_size=config.batch_size, shuffle=True)
 
-    device = (
-        "cuda"
-        if torch.cuda.is_available()
-        else "mps"
-        if torch.backends.mps.is_available()
-        else "cpu"
-    )
-    print(f"Using {device} device")
+    model = locals()[config.model](config).to(config.device)
 
-    model = RNN_GRU(config).to(device)
-    if config.load_model:
-        model.load_state_dict(torch.load(model_path))
+    if len(config.model_load_path):
+        print(f"Loading weights from {config.model_load_path}")
+        model.load_state_dict(torch.load(config.model_load_path))
+
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5)
+
+    print(f"Training with model {config.model}")
 
     for t in range(config.epochs):
         print(f"Epoch {t+1}\n-------------------------------")
@@ -337,6 +326,6 @@ if __name__ == "__main__":
         test_loop(valid_dataloder, model, loss_fn)
 
     print("Done!")
-
-    torch.save(model.state_dict(), model_path)
-    print("Model saved.")
+    if len(config.model_save_path):
+        torch.save(model.state_dict(), config.model_save_path)
+        print(f"Weights saved in {config.model_save_path}")
